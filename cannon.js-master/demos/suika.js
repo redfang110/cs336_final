6b96f9b16430075192dc063b293f662482499143
redfang110 <r3dfang110@gmail.com>
2023-12-05 23:39:53 -0600

    fucking mess

--- cannon.js-master/demos/suika.js
+++ cannon.js-master/demos/suika.js
@@ -1,118 +1,156 @@
 
-     /**
-      * A container filled with spheres.
-      */
+/*
+ * A container filled with spheres.
+ */
+var mouseX, mouseY, mouseZ;
+let camera, scene, renderer; // ThreeJS globals
+let world; // CannonJs world
+let gameEnded;
+let ballMats = [];
+let ballSizes = [];
+let floorMat, wallMat, red_planeMat;
+let widthsegments = 32, heightsegments = 32;
+let currentBall;
 
-     var demo = new CANNON.Demo();
-     var nx=4, ny=4;
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 'red' });
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphereMesh.position.set(0,.7,0);
+scene.add(sphereMesh);
 
-     // Test scalability - add scenes for different number of spheres
-     demo.addScene((nx*ny*4)+ " spheres", function(){   createContainer(demo,nx,ny,4);    });
-     demo.addScene((nx*ny*8)+ " spheres", function(){   createContainer(demo,nx,ny,8);    });
-     demo.addScene((nx*ny*15)+" spheres", function(){   createContainer(demo,nx,ny,15);   });
-     demo.addScene((nx*ny*20)+" spheres", function(){   createContainer(demo,nx,ny,20);   });
-     demo.addScene((nx*ny*25)+" spheres", function(){   createContainer(demo,nx,ny,25);   });
-     demo.addScene((nx*ny*30)+" spheres", function(){   createContainer(demo,nx,ny,30);   });
+function initMats() {
+  for (let i = 0; i < 5; i++) {
+    const color = new THREE.Color(`hsl(${30 + i * 10}, 100%, 50%)`);
+    const material = new THREE.MeshLambertMaterial({ color });
+    ballMats.push(material);
+  }
+  const color = new THREE.Color(0xffffff);
+  floorMat = new THREE.MeshLambertMaterial({ color });
+}
 
-     demo.start();
+function init() {
+  initMats();
+  ballSizes.push(0.5, 0.6, 1.0, 1.25, 2.0);
+  mouseX = 0; 
+  mouseY = 60;
+  mouseZ = 0;
 
-     function createContainer(demo,nx,ny,nz){
+  gameEnded = false;
+    
+  // Initialize CannonJS
+  world = new CANNON.World();
+  world.gravity.set(0, -10, 0); // Gravity pulls things down
+  world.broadphase = new CANNON.NaiveBroadphase();
+  world.solver.iterations = 40;
+    
+  // Initialize ThreeJs
+  const aspect = window.innerWidth / window.innerHeight;
+  const width = 10;
+  const height = width / aspect;
+    
+  camera = new THREE.OrthographicCamera(
+    width / -2, // left
+    width / 2, // right
+    height / 2, // top
+    height / -2, // bottom
+    0, // near plane
+    100 // far plane
+  );
+    
+  /*
+  // If you want to use perspective camera instead, uncomment these lines
+  camera = new THREE.PerspectiveCamera(
+    45, // field of view
+    aspect, // aspect ratio
+    1, // near plane
+    100 // far plane
+  );
+  */
+    
+  camera.position.set(4, 4, 4);
+  camera.lookAt(0, 0, 0);
+    
+  scene = new THREE.Scene();
+    
+  // Set up lights
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  scene.add(ambientLight);
+    
+  const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  dirLight.position.set(10, 20, 0);
+  scene.add(dirLight);
+    
+  // Set up renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animation);
+  document.body.appendChild(renderer.domElement);
 
-       // Create world
-       var world = demo.getWorld();
-       world.broadphase = new CANNON.SAPBroadphase(world); // Buggy?
+  currentBall = generateBall(Math.floor(Math.random() * 2));
+}
 
-       // Tweak contact properties.
-       world.defaultContactMaterial.contactEquationStiffness = 1e11; // Contact stiffness - use to make softer/harder contacts
-       world.defaultContactMaterial.contactEquationRelaxation = 2; // Stabilization time in number of timesteps
+function generateBall(index) {
+  // ThreeJS
+  const geometry = new THREE.SphereGeometry(ballSizes[index], widthsegments, heightsegments); 
+  const mesh = new THREE.Mesh(geometry, ballMats[index]);
+  mesh.position.set(mouseX, mouseY, mouseZ);
+  scene.add(mesh);
+    
+  // CannonJS
+  const shape = new CANNON.Sphere(ballSizes[index]);
+  let mass = 10; // If it shouldn't fall then setting the mass to zero will keep it stationary
+  mass *= ballSizes[index];
+  const body = new CANNON.Body({ mass, shape });
+  body.position.set(mouseX, mouseY, mouseZ);
+  world.addBody(body);
+    
+  return {
+    threejs: mesh,
+    cannonjs: body,
+    radius: ballSizes[index]
+  };
+}
 
-       // Max solver iterations: Use more for better force propagation, but keep in mind that it's not very computationally cheap!
-       world.solver.iterations = 10;
+function dropBall() {
 
-       world.gravity.set(0,0,-30);
+  generateBall(Math.floor(Math.random() * 2));
+}
 
-       // Since we have many bodies and they don't move very much, we can use the less accurate quaternion normalization
-       world.quatNormalizeFast = true;
-       world.quatNormalizeSkip = 8; // ...and we do not have to normalize every step.
+window.addEventListener('keydown', (event) => {
+  if (event.code === 'Space') {
+      gameStart = true;
+      dropBall();
+  }
 
-       // Materials
-       var stone = new CANNON.Material('stone');
-       var stone_stone = new CANNON.ContactMaterial(stone, stone, {
-         friction: 0.3,
-         restitution: 0.2
-       });
-       world.addContactMaterial(stone_stone);
+  if (event.code == 'ArrowLeft') {
+      mouseX -= 0.5;
+      if (mouseX <= -10){
+          mouseX = -10;
+      }
+      // sphereMesh.position.set(mouseX, 0.7, mouseZ);
+  }
 
-       // ground plane
-       var groundShape = new CANNON.Plane();
-       var groundBody = new CANNON.Body({ mass: 0, material: stone });
-       groundBody.addShape(groundShape);
-       world.addBody(groundBody);
-       demo.addVisual(groundBody);
+  if (event.code == 'ArrowRight') {
+      mouseX += 0.5;
+      if (mouseX >= 10){
+          mouseX = 10;
+      }
+      // sphereMesh.position.set(mouseX, 0.7, mouseZ);
+  }
 
-       // plane -x
-       var planeShapeXmin = new CANNON.Plane();
-       var planeXmin = new CANNON.Body({ mass: 0, material: stone });
-       planeXmin.addShape(planeShapeXmin);
-       planeXmin.quaternion.setFromAxisAngle(new CANNON.Vec3(0,1,0),Math.PI/2);
-       planeXmin.position.set(-5,0,0);
-       world.addBody(planeXmin);
-       demo.addVisual(planeXmin);
+  if (event.code == 'ArrowUp') {
+    mouseZ += 0.5;
+    if (mouseZ >= 10){
+      mouseZ = 10;
+    }
+    // sphereMesh.position.set(mouseX, 0.7, mouseZ);
+  }
 
-       
-       // Plane +x
-       var planeShapeXmax = new CANNON.Plane();
-       var planeXmax = new CANNON.Body({ mass: 0, material: stone });
-       planeXmax.addShape(planeShapeXmax);
-       planeXmax.quaternion.setFromAxisAngle(new CANNON.Vec3(0,1,0),-Math.PI/2);
-       planeXmax.position.set(5,0,0);
-       world.addBody(planeXmax);
+  if (event.code == 'ArrowDown') {
+    mouseZ -= 0.5;
+    if (mouseZ <= -10){
+      mouseZ = -10;
+    }
+    // sphereMesh.position.set(mouseX, 0.7, mouseZ);
+  }
 
-       // Plane -y
-       var planeShapeYmin = new CANNON.Plane();
-       var planeYmin = new CANNON.Body({ mass: 0, material: stone });
-       planeYmin.addShape(planeShapeYmin);
-       planeYmin.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
-       planeYmin.position.set(0,-5,0);
-       world.addBody(planeYmin);
-
-       // Plane +y
-       var planeShapeYmax = new CANNON.Plane();
-       var planeYmax = new CANNON.Body({ mass: 0,  material: stone });
-       planeYmax.addShape(planeShapeYmax);
-       planeYmax.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),Math.PI/2);
-       planeYmax.position.set(0,5,0);
-       world.addBody(planeYmax);
-
- //     const bottomMeshGeometry = new THREE.PlaneGeometry();
- // const bottomMeshMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
- // const bottomMesh = new THREE.Mesh(bottomMeshGeometry, bottomMeshMaterial);
- // bottomMesh.rotateY(Math.PI / 2.01);
- // bottomMesh.position.copy(planeYmax.position);
- // scene.add(bottomMesh);
-
-       // Create spheres
-       var rand = 0.1;
-       var h = 0;
-       var sphereShape = new CANNON.Sphere(1); // Sharing shape saves memory
-       world.allowSleep = true;
-       for(var i=0; i<nx; i++){
-         for(var j=0; j<ny; j++){
-           for(var k=0; k<nz; k++){
-             var sphereBody = new CANNON.Body({ mass: 7, material: stone });
-             sphereBody.addShape(sphereShape);
-             sphereBody.position.set(
-               i*2-nx*0.5 + (Math.random()-0.5)*rand,
-               j*2-ny*0.5 + (Math.random()-0.5)*rand,
-               1+k*2.1+h+(i+j)*0.0
-             );
-             sphereBody.allowSleep = true;
-             sphereBody.sleepSpeedLimit = 1;
-             sphereBody.sleepTimeLimit = 5;
-
-             world.addBody(sphereBody);
-             demo.addVisual(sphereBody);
-           }
-         }
-       }
-     }
+});
